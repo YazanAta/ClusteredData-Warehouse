f37d693780423cc4006d67a07d2d5fd93a1da410
Yazan Ata <yazan.ata.01@outlook.com>
2024-05-08 00:25:21 +0300

    improve validation and constructor usage

--- src/main/java/com/progresssoft/dto/DealRequestDTO.java
+++ src/main/java/com/progresssoft/dto/DealRequestDTO.java
@@ -3,20 +3,18 @@ package com.progresssoft.dto;
 import com.progresssoft.validation.CurrencyCode;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.sql.Timestamp;
 
 public class DealRequestDTO {
-    @NotBlank(message = "From currency ISO code is required")
-    @NotNull(message = "To currency ISO code is required")
+    @NotEmpty(message = "From currency ISO code is required")
     @CurrencyCode
     private String fromCurrencyIsoCode;
 
-    @NotBlank(message = "To currency ISO code is required")
-    @NotNull(message = "To currency ISO code is required")
+    @NotEmpty(message = "To currency ISO code is required")
     @CurrencyCode
     private String toCurrencyIsoCode;
 
@@ -27,6 +25,16 @@ public class DealRequestDTO {
     @NotNull(message = "Deal amount is required")
     @Positive(message = "Deal amount must be greater than 0")
     private Double dealAmount;
+
+    public DealRequestDTO(String fromCurrencyIsoCode, String toCurrencyIsoCode, @NotNull(message = "Deal timestamp is required") Timestamp dealTimestamp, @NotNull(message = "Deal amount is required") Double dealAmount) {
+        this.fromCurrencyIsoCode = fromCurrencyIsoCode;
+        this.toCurrencyIsoCode = toCurrencyIsoCode;
+        this.dealTimestamp = dealTimestamp;
+        this.dealAmount = dealAmount;
+    }
+
+    public DealRequestDTO() {
+    }
 
     public String getFromCurrencyIsoCode() {
         return fromCurrencyIsoCode;
