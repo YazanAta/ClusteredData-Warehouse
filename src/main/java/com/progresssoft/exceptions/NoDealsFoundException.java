bc82c57b9dd1049c555413d301c52e099e7da2e8
Yazan Ata <yazan.ata.01@outlook.com>
2024-05-08 00:39:47 +0300

    Add NoDealsFoundException class

---
+++ src/main/java/com/progresssoft/exceptions/NoDealsFoundException.java
@@ -0,0 +1,10 @@
+package com.progresssoft.exceptions;
+
+public class NoDealsFoundException extends RuntimeException {
+
+    public NoDealsFoundException(String message) {
+        super(message);
+    }
+
+}
+
